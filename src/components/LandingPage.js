eb69ea63f9e09767af8abf16a54c25bea6e410b1
Ashish Regmi <wanemphago@gmail.com>
2021-08-06 10:41:45 -0400

    Default React Logo removed

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from '../logo.svg';
+
 import '../LandingPage.css';
 import Graph from './Graph';
 
 const App = () => (
 	<div className="App">
 		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
 			<Graph />
 		</header>
 	</div>
